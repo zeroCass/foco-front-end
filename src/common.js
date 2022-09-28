d3a75340fbe48fd0453d1e09d371ec20b1c0a64c
zeroCass <mateusvalerio1@gmail.com>
2022-09-28 10:57:27 -0300

    [feat: commonjs] showError functions now shows whatever type of error msg

--- src/common.js
+++ src/common.js
@@ -4,10 +4,18 @@ const server = Platform.OS === 'ios' ? 'http://54.233.129.48:3000' : 'http://54.
 // const server = 'http://localhost:3000'
 
 const showError = (err) => {
+    console.log('showErr', err)
     if (err.response && err.response.data) {
         Alert.alert(`Ops!Algo deu errado: ${err.response.data}`)
+        return 
     }
-    Alert.alert(`Ops!Algo deu errado: ${err.response}`)
+    if (err.response) {
+        Alert.alert(`Ops!Algo deu errado: ${err.response}`)
+        return
+    }
+
+    Alert.alert(`Ops!Algo deu errado: ${err}`)
+        
 }
 
 const showSucess = (msg) => {
